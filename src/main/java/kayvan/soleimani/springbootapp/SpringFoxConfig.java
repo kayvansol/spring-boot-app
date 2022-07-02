ab0e75470a8bfa5622552f02e412adc32c776d43
Kayvan Soleimani <53554624+kayvansol@users.noreply.github.com>
2022-07-02 09:18:37 +0430

    Update SpringFoxConfig.java

--- src/main/java/kayvan/soleimani/springbootapp/SpringFoxConfig.java
+++ src/main/java/kayvan/soleimani/springbootapp/SpringFoxConfig.java
@@ -9,6 +9,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SpringFoxConfig {
+    
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
