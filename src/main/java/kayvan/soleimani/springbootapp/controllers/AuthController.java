121789f051e50db4bf98ff049d30aa4a5f087f1f
Kayvan Soleimani <53554624+kayvansol@users.noreply.github.com>
2022-07-07 23:49:21 +0430

    Update AuthController.java

--- src/main/java/kayvan/soleimani/springbootapp/controllers/AuthController.java
+++ src/main/java/kayvan/soleimani/springbootapp/controllers/AuthController.java
@@ -52,6 +52,7 @@ public class AuthController {
         new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
     SecurityContextHolder.getContext().setAuthentication(authentication);
+    
     String jwt = jwtUtils.generateJwtToken(authentication);
     
     UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();    
